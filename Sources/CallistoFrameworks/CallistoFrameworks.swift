38fe85f4eb3700424854022663aed1f7ede50928
Jay Lyerly <jay@oakcity.io>
2022-02-08 09:18:21 -0500

    Restore code.

--- Sources/CallistoFrameworks/CallistoFrameworks.swift
+++ Sources/CallistoFrameworks/CallistoFrameworks.swift
@@ -2,8 +2,8 @@ import Foundation
 
 public class CallistoFrameworks {
     
-//    static var pythonLibraryUrl: URL? {
-//        Bundle.module.url(forResource: "Library", withExtension: nil)
-//    }
+    static var pythonLibraryUrl: URL? {
+        Bundle.module.url(forResource: "Library", withExtension: nil)
+    }
     
 }
